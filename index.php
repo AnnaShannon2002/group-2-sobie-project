25892df40a170d9b7cb99354f6e74e77c942a93c
Anna Shannon <149623553+AnnaShannon2002@users.noreply.github.com>
2025-03-11 20:58:47 -0500

    Added a link to the readdb.ddl file

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <html>
   <body>
     Welcome to the website!
-    <a href="register.php">Next page</a>
+    <h2>Main Menu</h2>
+        <ul>
+            <li><a href="register.php">Register</a></li>
+            <li><a href="readdb.php">Read Database</a></li>
+        </ul>
   </body>
 </html>
